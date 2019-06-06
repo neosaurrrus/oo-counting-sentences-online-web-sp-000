0d7ff648ed0e99f2e1fc2da58572a73a4d73c3cf
Lukie Kang <lukiek+f@gmail.com>
2019-06-06 16:08:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,11 +11,11 @@ class String
   end
 
   def question?
-    self.end == "?" ? true : false
+    self.end_with?(".")
   end
 
   def exclamation?
-    self.end == "!" ? true : false
+    self.end_with?(".")
   end
 
   def count_sentences

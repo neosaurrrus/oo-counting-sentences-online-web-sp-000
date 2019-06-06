4a3618edda5eb1ce9f219425de8d17fc0f89166b
Lukie Kang <lukiek+f@gmail.com>
2019-06-06 16:03:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
+
+
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end == "." ? true : false
   end
 
   def question?
-
+    self.end == "?" ? true : false
   end
 
   def exclamation?
-
+    self.end == "!" ? true : false
   end
 
   def count_sentences
-
+    count = 0
+    #split by periods, exclamations, questions
+    #loop through arr of splits
+    # if arr is longer than 0 count up
+    return count
   end
 end
